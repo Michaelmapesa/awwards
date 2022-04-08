c075f2a70e33f1730ce9ddb630ed55b0ba21b7d5
michael mapesa <michaelmapesa93@gmail.com>
2022-04-08 19:36:26 +0300

    add index function in the model

--- awwarde/urls.py
+++ awwarde/urls.py
@@ -5,7 +5,7 @@ app_name = "main"
 
 
 urlpatterns = [
-    path("", views.display_images, name="homePage"),
+    path("", views.index, name="homePage"),
     path("register", views.register_request, name="register"),
     path("login", views.login_request, name="login")
 ]
