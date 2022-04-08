5e73531dc759ea8e02f8b70c41e74567dc16b60b
michael mapesa <michaelmapesa93@gmail.com>
2022-04-08 07:41:02 +0300

    add login file

--- awwarde123/settings.py
+++ awwarde123/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'awwarde',
+    'crispy_forms',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
