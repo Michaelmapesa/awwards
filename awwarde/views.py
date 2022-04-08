22a2b3cb665a7dc0b15366d179bd11f95ba13e3f
michael mapesa <michaelmapesa93@gmail.com>
2022-04-08 08:26:41 +0300

    addbase.html file

--- awwarde/views.py
+++ awwarde/views.py
@@ -21,20 +21,20 @@ def register_request(request):
 	form = NewUserForm()
 	return render (request=request, template_name="main/register.html", context={"register_form":form})
 
-
 def login_request(request):
 	if request.method == "POST":
 		form = AuthenticationForm(request, data=request.POST)
 		if form.is_valid():
-			username = form.cleaned_data.get("username")
-			password = form.cleaned_data.get("password")
-			user = authenticate(username=username,password=password)
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
 			if user is not None:
 				login(request, user)
-				messages.info(request,f"You are now logged in as {username}.")
-				return redirect("homepage")
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("main:homepage")
 			else:
 				messages.error(request,"Invalid username or password.")
-		form = AuthenticationForm()
-		return render(request=request, template_name="main/login.html",context={"login_form":form})
-
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="main/login.html", context={"login_form":form})
