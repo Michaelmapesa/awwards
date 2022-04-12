101da273b75d21cbbf3141a9d0187042c671813e
michael mapesa <michaelmapesa93@gmail.com>
2022-04-12 08:53:21 +0300

    change db

--- awwarde123/settings.py
+++ awwarde123/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'awwarde123.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'awward',
+        'NAME': 'ipp3',
         'USER': 'moringaaccess',
         'PASSWORD':'access',
     }
